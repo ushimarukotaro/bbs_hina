cf02765b68ce45b2efa9ca8496d553abeabb6f9e
ushimaru <uxsxmxr14@gmail.com>
2020-03-30 16:41:33 +0900

    add_config

---
+++ config/config.php
@@ -0,0 +1,23 @@
+<?php
+ini_set("display_errors", 1);
+define("DSN", "mysql:host=localhost;charset=utf8;dbname=bbs");
+define("DB_USERNAME", "root");
+define("DB_PASSWORD", "root");
+//define("SITE_URL", "http://" . $_SERVER["HTTP_HOST"] . "/public.html");
+
+define("SITE_URL", "http://" . $_SERVER["HTTP_HOST"] . "/bbs_hina/public_html"); // 栗岩追記：この部分はご自身の環境によって書き換えが必要です。public_htmlまでのパスがどうなっているかによって　$_SERVER["HTTP_HOST"] 以降の記述が変わります。
+
+require_once(__DIR__ . "/../lib/Controller/functions.php");
+require_once(__DIR__ . "/autoload.php");
+session_start();
+
+// spl_autoload_register(function ($class) {
+//   $prefix = "Bbs\"";
+//   if (strpos($class, $prefix) === 0) {
+//     $className = substr($class, strlen($prefix));
+//     $classFilePath = __DIR__ . "/../" . str_replace("\"", "/", $className) . ".php";
+//     if (file_exists($classFilePath)) {
+//       require $classFilePath;
+//     }
+//   }
+// });
