82033688acbacc551124461e0bd3223261a053ec
ushimaru <uxsxmxr14@gmail.com>
2020-05-18 17:07:59 +0900

    add_my_page

--- public_html/js/bbs.js
+++ public_html/js/bbs.js
@@ -1,2 +1,24 @@
+'use strict'
 $(function () {
+  $('input[type=file]').change(function () {
+    var file = $(this).prop('files')[0];
+
+    // 画像以外は処理を停止
+    if (!file.type.match('image.*')) {
+      // クリア
+      $(this).val('');
+      $('.imgfile').html('');
+      return;
+    }
+
+    // 画像表示
+    var reader = new FileReader();
+    reader.onload = function () {
+      var img_src = $('').attr('src', reader.result);
+      $('.imgfile').html(img_src);
+      $('.imgarea').removeClass('noimage');
+    }
+    reader.readAsDataURL(file);
+  });
 });
+
